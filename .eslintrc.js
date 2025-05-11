7597582388918a90c5e2af08c23f2c7e7e236cc1
altramax <ajayiezekiel559@gmail.com>
2025-05-11 20:44:41 +0100

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   extends: [
-    "next/core-web-vitals",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended"
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    '@typescript-eslint/recommended',
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "no-console": "warn",
-    "react/prop-types": "off"
-  }
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
 };
