5dd6a0d3e40fc51944efc44a998d86b0ef45036b
altramax <ajayiezekiel559@gmail.com>
2025-06-18 00:20:33 +0100

    refactor

--- src/app/(private-route)/patients/prescriptions/page.tsx
+++ src/app/(private-route)/patients/prescriptions/page.tsx
@@ -1,5 +1,3 @@
-import PatientsCareTemplate from '@/src/components/templates/patient-care/patient-care-template';
-
 export default function Page() {
-  return <PatientsCareTemplate />;
+  return <div></div>;
 }
